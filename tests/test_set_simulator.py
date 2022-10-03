a8051edc8143282abdee944e4e222a45a7f7ac99
Mitchell Krawiec-Thayer <contact@mitchellpkt.com>
2022-10-02 21:06:22 -0600

    tests: for set counter

--- tests/test_set_simulator.py
+++ tests/test_set_simulator.py
@@ -1,5 +1,10 @@
 import pytest
-from set_simulator.main import generate_deck, is_valid_set, standard_canonical_order
+from set_simulator.main import (
+    generate_deck,
+    is_valid_set,
+    standard_canonical_order,
+    count_valid_sets,
+)
 
 
 def test_order():
@@ -42,3 +47,31 @@ def test_is_set_bad():
             (3, "purple", "solid", "diamond"),
         ]
     )
+
+
+def test_count_valid_sets():
+    expected: int = 6
+    observed: int = count_valid_sets(
+        [
+            (2, "green", "striped", "diamond"),
+            (1, "red", "open", "diamond"),
+            (3, "purple", "solid", "diamond"),
+            (1, "purple", "open", "diamond"),
+        ],
+        verbose=True,
+    )
+    assert expected == observed
+
+
+def test_count_valid_sets_with_none():
+    expected: int = 0
+    observed: int = count_valid_sets(
+        [
+            (2, "green", "striped", "diamond"),
+            (1, "red", "open", "diamond"),
+            (2, "purple", "solid", "diamond"),
+            (1, "purple", "open", "diamond"),
+        ],
+        verbose=True,
+    )
+    assert expected == observed
